3107dfeab812eed915b52cb3f0721205b709e514
Man Wai Wong <william.manwai@gmail.com>
2017-09-26 23:56:59 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// create new componenet
+// component use to create HTML
+const App = function(){
+  return <div>屌</div>;
+}
+//take this compoennt generated HTML and put it on the page
+
+React.render(App);
