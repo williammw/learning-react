c729e41b17cfa8cfbe91a68c7d28876853e07dec
WONG <wmwwong@legco.hksar>
2017-09-27 11:10:46 +0800

    adding videoList and ListItem

---
+++ src/components/video_list.js
@@ -0,0 +1,25 @@
+//10.1
+import React from 'react';
+// 11.1 
+import VideoListItem from './video_list_item';
+const VideoList = (props) => {
+    // 10.3 explain of the props
+    //(props) const videos = props.videos; <--explaination
+
+    // 12.2 
+    const VideoItems = props.videos.map((video) => {
+        // 12.3 ((.)(.)) we're using google youtube api and adding a key using etag of response data.
+        // version 1
+        //return <VideoListItem video={video} />
+        // adding key [=> key={video.etag} <=]
+        return <VideoListItem key={video.etag} video={video} />
+        // 12.3 the warrning (Each child in an array or iterator should have a unique "key" prop.)
+    });
+    return (
+        <ul className="col-md-4 list-group">
+        {/* 11.1 */}
+            {VideoItems}
+        </ul>
+    );
+};
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 // actuallt render the DOM 
 import ReactDom from 'react-dom';
-/*!!!!!!*/
+// 8.1 import youtube search apu
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-/*!!!!!!!*/
-// ReactDom 
+// 10.2 import video list
+import VideoList from './components/video_list';
+// ReactDom
 // create new componenet
 // component use to create HTML
-const API_KEY = 'AIzaSyDDShiBJjwdbGn-wwgNlj9yvQeuiCy5nYk';
+const API_KEY = "AIzaSyDDShiBJjwdbGn-wwgNlj9yvQeuiCy5nYk";
+
+
+// 9.1 refactor function component to class based component
+/*
 const App = () => {
   // @when we create a component, we create a class of compoenet
   return (<div>
     <SearchBar />
   </div>);
+}
+*/
+
+//9.2 
+class App extends Component {
+  // @when we create a component, we create a class of compoenet
+  constructor(props){
+    super(props);
+    this.state = {videos:[]};
+
+    
+    //8.2 -> 9.3 (move 8.2 outside the function object into class based component)
+    YTSearch({key:API_KEY, term : 'Jason' }, (videos) => {
+      // if key and value are identical term "videos and (videos) =>" ES6 can make it o videos
+      this.setState({videos})
+      // same as this.setState({videos:videos})
+    });
+
+  }
+  render(){
+    //10.4 class based component can use props everywhere.
+    //this.props.xxx
+    return (
+    <div>
+      <SearchBar />
+      <VideoList videos={this.state.videos} />
+    </div>);
+  }
 }
 //take this compoennt generated HTML and put it on the page
 // wrap App by < /> to make it become from classes to component 
